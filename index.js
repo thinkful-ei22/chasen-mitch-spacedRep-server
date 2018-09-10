d26eca35a9097740d948c6d41c9599dd43f70a71
MitchGianoni <mitchgianoni@yahoo.com>
2018-09-10 15:54:50 -0400

    added boilerplate to index.js, installed some dependencies

--- index.js
+++ index.js
@@ -1,26 +1,60 @@
-'use strict';
-
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-
+const passport = require('passport');
+const {localStrategy} = require('./passport/local');
+const {jwtStrategy} = require('./passport/jwt');
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
-// const {dbConnect} = require('./db-knex');
+const authRouter = require('./routes/auth');
+const usersRouter = require('./routes/users');
 
+
+// Create an Express application
 const app = express();
 
+// Log all requests unless in test
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
   })
 );
 
+// Enable CORS support
 app.use(
   cors({
     origin: CLIENT_ORIGIN
   })
 );
+
+// Parse request
+app.use(express.json());
+
+// Passport Strategies
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+// Mount Routers
+
+app.use('/api/auth', authRouter);
+app.use('/api/users', usersRouter);
+
+// Custom 404 Not Found route handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Custom Error Handler
+app.use((err, req, res, next) => {
+  if(err.status) {
+    const errBody = Object.assign({}, err, { message: err.message });
+    res.status(err.status).json(errBody);
+  } else {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Server Error '});
+  }
+});
 
 function runServer(port = PORT) {
   const server = app
