12c26444b6ad91fa78f421394b7c12896efc2e5d
MitchGianoni <mitchgianoni@yahoo.com>
2018-09-13 16:42:54 -0400

    al-gore-rhythm works

--- routes/questions.js
+++ routes/questions.js
@@ -47,6 +47,7 @@ router.post('/', (req, res, next) => {
   // extract from request
   const {guess} = req.body;
   const userId = req.user.id;
+  let feedback;
 
   User.findById(userId)
     .populate('questions.qData')
@@ -61,17 +62,16 @@ router.post('/', (req, res, next) => {
       if (guess.toLowerCase() === answer) {
         question.solved = true;
         question.memValue *= 2;
+        feedback = true;
       } else {
         question.memValue = 1;
+        feedback = false;
       }
 
       // change head to the value of next
-      if (user.head === null) {
-        user.head = 0;
-      } else {
-        user.head = question.next;
-      }
+      user.head = question.next;
 
+      // rearrange node order
       let currentQuestion = question;
       for (let i = 0; i < question.memValue; i++) {
         const nextIndex = currentQuestion.next;
@@ -84,7 +84,7 @@ router.post('/', (req, res, next) => {
       currentQuestion.next = index;
       return user.save();
     })
-    .then(() => res.status(200).json({}));
+    .then(user => res.status(200).json({feedback, user}));
 
 });
 
